644a75f583ee2feae66df365c6182bec4feae036
Guillermo Obando <guillermoj.obando@gmail.com>
2024-09-24 18:34:46 +0000

    BackEnd 80%

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard();
 };
+
+function generateCard() {
+  const numberRan = document.querySelector("#card-numbers");
+  const icono = document.querySelector("#card-icons");
+  const ico = document.querySelector("#card-icon");
+
+  const icons = ["♦", "♥", "♠", "♣"];
+  const numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jack",
+    "♛",
+    "♚",
+    "Ace"
+  ];
+
+  const randomIcons = icons[Math.floor(Math.random() * icons.length)];
+  const randomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
+
+  numberRan.innerHTML = randomNumbers;
+  icono.innerHTML = randomIcons;
+  ico.innerHTML = randomIcons;
+}
